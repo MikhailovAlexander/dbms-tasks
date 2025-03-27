46b110e3e1029ebcaab153c85f6b808b3743a537
MikhailovAlexander <avmikhaylov_3@edu.hse.ru>
2025-03-28 00:39:10 +0500

    fix

--- init/init-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Скрипт для инициализации базы данных для приложения.
-
-set -e
-
-echo run init script
-
-echo run sql script
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/sql/init.sql
